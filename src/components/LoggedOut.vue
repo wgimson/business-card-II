d07c996bb46842006b077ddd78ae1e37478d4ed5
wgimson <wgimson@gmail.com>
2021-02-11 03:28:14 -0500

    UI updates and version bumps

--- src/components/LoggedOut.vue
+++ src/components/LoggedOut.vue
@@ -2,7 +2,7 @@
   <div class="custom-contain container">
     <div class="card">
       <div class="card-content center-align">
-        <h1>Log back in to Chat!!!</h1>
+        <h1>Log back in to Macy and Billy's Business Card O Rama!!!</h1>
       </div>
     </div>
   </div>
